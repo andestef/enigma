dd9948f41dcab420b8a1c6f01d42eca6ab59d96d
mathstar13 <alexander.destefano@gmail.com>
2024-11-09 11:53:55 -0500

    Fixed Bug In generator that was only generating half of the required letters

--- generator.py
+++ generator.py
@@ -19,7 +19,7 @@ def generate(gen):
         open('sets/rotors/current_settings.json','w').write(dumps([choice(validnumbers),choice(validnumbers),choice(validnumbers)],indent=4, sort_keys=True))
     elif gen == 'a' or gen == 'b' or gen == 'c' or gen == 'reflector':
         c = 0
-        for a in alphabet:
+        for a in list(alphabet):
             c+=1
             chr = choice(alphabet)
             data[c] = [a,chr]
